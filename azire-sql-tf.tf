523e05af59b8f090f92b415644405623f3d33154
acvallakati <153590051+acvallakati@users.noreply.github.com>
2025-10-07 18:32:03 +0530

    Create azire-sql-tf.tf

---
+++ azire-sql-tf.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rsg" {
+  name     = "sql-rsg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "sql-server" {
+  name                         = "test-sqlserver"
+  resource_group_name          = azurerm_resource_group.rsg.name
+  location                     = azurerm_resource_group.rsg.location
+  version                      = "12.0"
+  administrator_login          = "testadmin"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "sql-db" {
+  name         = "test-sql-db"
+  server_id    = azurerm_mssql_server.sql-server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
